4a2387c279eed39c1d4617eedd6ff5262af789e8
cicerotcv <49568618+cicerotcv@users.noreply.github.com>
2019-09-17 18:12:32 -0300

    functions

--- script.py
+++ script.py
@@ -18,50 +18,93 @@ auth = tweepy.OAuthHandler(data['consumer_key'], data['consumer_secret'])
 auth.set_access_token(data['access_token'], data['access_token_secret'])
 
 
-#Produto escolhido:
-produto = 'depressao'
-#Filtro de língua, escolha uma na tabela ISO 639-1.
-lang = 'pt'
 
-
-
-
-
-planilha = pd.read_excel('DataBase.xlsx', "Sheet1")
-#Cria um objeto para a captura
-api = tweepy.API(auth)
-#Inicia a captura, para mais detalhes: ver a documentação do tweepy
-i = 0
-Limite = 200
-novas_mensagens = 0
-while i < Limite:
-    for msg in tweepy.Cursor(api.search, q="{0} -filter:retweets".format(produto), lang=lang, tweet_mode="extended", count=10).items():
-        new_msg = { 'User Name': msg.user.name,
-                    'Tweet Created At': msg.created_at,
-                    'Tweet Text': msg.full_text.lower(),
-                    'Relevância': '',
-                    'User Location': msg.user.location,
-                    'Phone Type': msg.source,
-                    'Favorite Count': msg.favorite_count,
-                    'Retweets':msg.retweet_count
-                    }
-        i += 1
-        if  new_msg["Tweet Text"] not in planilha["Tweet Text"].values:
-            writer = pd.ExcelWriter('DataBase.xlsx', engine='xlsxwriter')
-            planilha = planilha.append(pd.DataFrame([new_msg]), ignore_index=True)
-            planilha.to_excel(excel_writer = writer, index = False)
-            writer.save()   
-            print("[{2}]\t{0}\t{1}".format(new_msg["User Name"], new_msg["Tweet Text"], i))
-            novas_mensagens +=1
-            
-        else:
-            print("[{}] Tweet repetido".format(i))
-        print('[{}] -=-=-=-=-= aguardando 5 segundos =-=-=-=-=-\n\n'.format(i))
-        time.sleep(5)
-        if i >= Limite:
-            break
-    print("[Encerrando Programa]")
-print(planilha.loc[:][["User Name","Tweet Text"]].sort_values(by="User Name"))
-print("{} new messages in DataBase.xlsx".format(novas_mensagens))
+def download_tweets(quantidade, autorizacao):
+    planilha = pd.read_excel('DataBase.xlsx', "Sheet1")
+    #Cria um objeto para a captura
+    api = tweepy.API(auth)
+    #Inicia a captura, para mais detalhes: ver a documentação do tweepy
+    i = 0
+    novas_mensagens = 0
+    while i < quantidade:
+        repeater = 0
+        for msg in tweepy.Cursor(api.search, q="{0} -filter:retweets".format("depressao"), lang='pt', tweet_mode="extended").items():
+            new_msg = { 'User Name': msg.user.name,
+                        'Tweet Created At': msg.created_at,
+                        'Tweet Text': msg.full_text.lower(), 
+                        'Relevância': '',
+                        'User Location': msg.user.location,
+                        'Phone Type': msg.source,
+                        'Favorite Count': msg.favorite_count,
+                        'Retweets':msg.retweet_count
+                        }
+            i += 1
+            if  new_msg["Tweet Text"] not in planilha["Tweet Text"].values:
+                writer = pd.ExcelWriter('DataBase.xlsx', engine='xlsxwriter')
+                planilha = planilha.append(pd.DataFrame([new_msg]), ignore_index=True)
+                planilha.to_excel(excel_writer = writer, index = False)
+                writer.save()   
+                print("[{2}]\n{0}\n{1}".format(new_msg["User Name"], new_msg["Tweet Text"], i))
+                novas_mensagens +=1
+                
+            else:
+                print("[{}] Tweet repetido".format(i))
+            print('[{}] -=-=-=-=-= aguardando 4.5 segundos =-=-=-=-=-\n\n'.format(i))
+            time.sleep(4.5)
+            if i >= quantidade or repeater >= 10:
+                print("[Encerrando Programa]")
+                break
+    print(planilha.loc[:][["User Name","Tweet Text"]].sort_values(by="User Name").head(10))
+    print("{} new messages in DataBase.xlsx".format(novas_mensagens))
+    return True
     
+def return_tweet(autorizacao, quantidade):
+    """return_tweet(autorizacao, quantidade)"""
+    api = tweepy.API(autorizacao)
+    i = 0
+    anterior = {'User Name': 'foo',
+                'Tweet Created At': 'foo','Tweet Text': 'foo','Relevância': 'foo',
+                'User Location': 'foo','Phone Type': 'foo',
+                'Favorite Count': 'foo','Retweets':'foo'}
+    novas_mensagens = 0
+    while i < quantidade:
+        for msg in tweepy.Cursor(api.search, q="{0} -filter:retweets".format("depressao"), lang='pt', tweet_mode="extended").items():
+            new_msg = { 'User Name': msg.user.name,
+                        'Tweet Created At': msg.created_at,
+                        'Tweet Text': msg.full_text.lower(),
+                        'Relevância': '',
+                        'User Location': msg.user.location,
+                        'Phone Type': msg.source,
+                        'Favorite Count': msg.favorite_count,
+                        'Retweets':msg.retweet_count
+                        }
+            i += 1
+            if  new_msg["Tweet Text"] != anterior["Tweet Text"]:
+                anterior = new_msg
+                print("[{2}]\n{0}\n{1}".format(new_msg["User Name"], new_msg["Tweet Text"], i))
+                novas_mensagens +=1
+                
+            else:
+                print("[***] Tweet repetido".format(i))
+                repeater += 1
+            print('\t -=-=-=-=-= aguardando 4.5 segundos =-=-=-=-=-\n\n'.format(i))
+            time.sleep(4.5)
+            if i >= quantidade or repeater >= 10:
+                break
+        print("[Fim do Programa]")
+
+
+def remove_http(lista_de_listas):
+    lista = lista_de_listas
+    for i in range(len(lista)):
+        for j in range(len(lista[i])):
+            if 'HTTP' in lista[i][j].capitalize():
+                lista[i][j] = "http"
     
+    for i in range(len(lista)):
+        while lista[i].count("http") != 0:
+            lista[i].remove("http")
+
+    return lista
+
+download_tweets(200, auth)
