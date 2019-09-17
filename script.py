213fd971e5a6ccd49212bc476a91bb41638e525b
cicerotcv <49568618+cicerotcv@users.noreply.github.com>
2019-09-17 04:15:42 -0300

    o script roda durante 15 minutos e pode coletar até 200 novas mensagens

--- script.py
+++ script.py
@@ -7,6 +7,7 @@ import os.path
 import pandas as pd
 import json
 import time
+from random import shuffle
 
 #leitura do arquivo no formato JSON
 with open('auth.pass') as fp:    
@@ -31,9 +32,10 @@ planilha = pd.read_excel('DataBase.xlsx', "Sheet1")
 api = tweepy.API(auth)
 #Inicia a captura, para mais detalhes: ver a documentação do tweepy
 i = 0
-Limite = 50
+Limite = 200
+novas_mensagens = 0
 while i < Limite:
-    for msg in tweepy.Cursor(api.search, q="{0} -filter:retweets".format(produto), lang=lang, tweet_mode="extended", count=200).items():
+    for msg in tweepy.Cursor(api.search, q="{0} -filter:retweets".format(produto), lang=lang, tweet_mode="extended", count=10).items():
         new_msg = { 'User Name': msg.user.name,
                     'Tweet Created At': msg.created_at,
                     'Tweet Text': msg.full_text.lower(),
@@ -44,17 +46,22 @@ while i < Limite:
                     'Retweets':msg.retweet_count
                     }
         i += 1
-        if new_msg["Tweet Text"] not in planilha["Tweet Text"]:
+        if  new_msg["Tweet Text"] not in planilha["Tweet Text"].values:
             writer = pd.ExcelWriter('DataBase.xlsx', engine='xlsxwriter')
             planilha = planilha.append(pd.DataFrame([new_msg]), ignore_index=True)
             planilha.to_excel(excel_writer = writer, index = False)
             writer.save()   
-            print("new_message: {0}".format(new_msg["Tweet Text"]))
-            print('[{}] aguardando 5 segundos'.format(i))
-            time.sleep(5)
+            print("[{2}]\t{0}\t{1}".format(new_msg["User Name"], new_msg["Tweet Text"], i))
+            novas_mensagens +=1
+            
         else:
-            time.sleep(2)
+            print("[{}] Tweet repetido".format(i))
+        print('[{}] -=-=-=-=-= aguardando 5 segundos =-=-=-=-=-\n\n'.format(i))
+        time.sleep(5)
         if i >= Limite:
             break
+    print("[Encerrando Programa]")
+print(planilha.loc[:][["User Name","Tweet Text"]].sort_values(by="User Name"))
+print("{} new messages in DataBase.xlsx".format(novas_mensagens))
     
     
